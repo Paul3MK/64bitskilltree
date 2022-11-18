bc128670432c00d5d283a22210220d0c4a29977f
paul3mk <hk.paul1999@gmail.com>
2022-11-18 09:31:35 +0000

    refactor: Rename Himalaya API Caller

---
+++ backend/himalayasAPICaller.py
@@ -0,0 +1,45 @@
+import requests as re
+import datetime
+from trie import TrieNode
+
+skill_dict = {1: "Python", 2: "Vertex", 3: "java"}
+
+def jobGetter():
+    job = re.get("https://himalayas.app/jobs/api?limit=5")
+
+    site_dump = job.json()
+
+    job_directory = []
+
+
+    for j in site_dump["jobs"]:
+
+        job_directory.append(
+            {
+                "job-title": j["title"],
+                "employer": j["companyName"],
+                "description": j["description"],
+                "published-on": datetime.datetime.fromtimestamp(j["pubDate"]).strftime("%c"),
+                "link": j["guid"]
+            }
+        )
+
+    return job_directory
+
+def jobChecker(job_array):
+
+    result_set = []
+
+    t = TrieNode()
+
+    for skill in skill_dict:
+        t.addPhrase(t, skill_dict[skill], skill)
+
+    for job in job_array:
+        result_set.append(t.findPhrases(t, job["description"]))
+        
+    return result_set
+
+job_list = jobGetter()
+print(job_list)
+print(jobChecker(job_list))

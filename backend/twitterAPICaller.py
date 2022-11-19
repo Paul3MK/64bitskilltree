e1d0a0f0964dd8544cde2601360f145a4c611ebc
paul3mk <hk.paul1999@gmail.com>
2022-11-19 11:06:56 +0000

    feat: Add GET Tweet Count logic

---
+++ backend/twitterAPICaller.py
@@ -0,0 +1,28 @@
+import requests
+from os import environ as env
+from dotenv import load_dotenv
+
+def getAllTweetCounts(skill):
+
+    load_dotenv()
+    BEARER_TOKEN = env['TWITTER_BEARER_TOKEN']
+
+    excluding = ["-pay ", "-bot ", "-assignment ", "-homework ", "-writer "]
+
+    query = ""
+
+    for term in excluding:
+        query += term
+
+    query += skill
+
+    params = {
+        "query": query
+    }
+
+    headers = {"Authorization": "Bearer "+BEARER_TOKEN}
+
+    tweet_count = requests.get(url="https://api.twitter.com/2/tweets/counts/recent", params=params, headers=headers)
+    res = tweet_count.json()
+
+    return res["meta"]["total_tweet_count"]

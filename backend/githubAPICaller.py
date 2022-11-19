96e519c881c6c59e1fc2613120c721246a11bbb0
paul3mk <hk.paul1999@gmail.com>
2022-11-19 23:14:05 +0000

    feat: Add GET GitHub calls for repos and issues

---
+++ backend/githubAPICaller.py
@@ -0,0 +1,62 @@
+import requests
+import asyncio
+import aiohttp
+from dotenv import load_dotenv
+from os import environ as env
+import datetime as dt
+import urllib.parse
+
+base_url = "https://api.github.com/search/"
+
+async def getRepositories(base_url, lang):
+
+    load_dotenv()
+
+    now = dt.datetime.now(dt.timezone.utc)
+    month_ago = now - dt.timedelta(days=30)
+    
+    today = now.strftime("%Y-%m-%d")
+    last_month = month_ago.strftime("%Y-%m-%d")
+
+    params={
+        "q":"stars:>0 pushed:>{} language:{}".format(last_month, lang),
+        "per_page":1
+    }
+
+    headers={"Authorization": "Bearer "+env['GITHUB_TOKEN']}
+
+    async with aiohttp.ClientSession() as session:
+
+        repos = await session.get(base_url+"repositories", params=params, headers=headers)
+
+        response = await repos.json()
+
+        return response["total_count"]
+
+async def getIssues(base_url, lang):
+    load_dotenv()
+
+    now = dt.datetime.now(dt.timezone.utc)
+    month_ago = now - dt.timedelta(days=30)
+    
+    today = now.strftime("%Y-%m-%d")
+    last_month = month_ago.strftime("%Y-%m-%d")
+
+    params={
+        "q":"comments:>5 updated:>{} language:{}".format(last_month, lang),
+        "per_page":1
+    }
+
+    headers={"Authorization": "Bearer "+env['GITHUB_TOKEN']}
+
+    async with aiohttp.ClientSession() as session:
+
+        repos = await session.get(base_url+"issues", params=params, headers=headers)
+
+        response = await repos.json()
+
+        return response["total_count"]
+
+
+if __name__ == "__main__":
+    pass

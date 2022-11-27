432a0c130bf51e9db7befac46b328552ba9e7aec
paul3mk <hk.paul1999@gmail.com>
2022-11-27 06:07:23 +0000

    refactor: Change logic for GET for robust db load

--- backend/himalayasAPICaller.py
+++ backend/himalayasAPICaller.py
@@ -1,33 +1,88 @@
 import requests as re
 import datetime
 from trie import TrieNode
+import json
+import time
+import skills
 
-skill_dict = {1: "Python", 2: "Vertex", 3: "java"}
+d = {}
 
-def jobGetter():
-    job = re.get("https://himalayas.app/jobs/api?limit=5")
-
-    site_dump = job.json()
-
-    job_directory = []
+skill_dict = skills.fullList(d)
+skill_count = {}
+for i in skill_dict:
+    skill_count[i] = 0
 
 
-    for j in site_dump["jobs"]:
+def jobGetter(state):
 
-        job_directory.append(
-            {
-                "job-title": j["title"],
-                "employer": j["companyName"],
-                "description": j["description"],
-                "published-on": datetime.datetime.fromtimestamp(j["pubDate"]).strftime("%c"),
-                "link": j["guid"]
-            }
-        )
+    if state == "offline":
+        with open("him.json", "r") as f:
+            site_dump = json.load(f)
 
-    return job_directory
+            return site_dump["jobs"]
+    elif state == "online":
+        params = {
+            "limit": 200,
+            "offset" : 0 
+        }
+
+        jobs = re.get("https://himalayas.app/jobs/api", params=params)
+
+        initial_dump = jobs.json()
+
+        params["offset"] = 200
+
+        total_jobs = initial_dump["total_count"]
+
+        max_offset = total_jobs - (total_jobs % 200)
+
+        final_limit = total_jobs % 200
+
+        for i in range(2, int(max_offset/200) + 1):
+            more_jobs = re.get("https://himalayas.app/jobs/api", params=params)
+
+            params["offset"] = i * params["limit"]
+
+            new_dump = more_jobs.json()
+
+            initial_dump["jobs"].extend(new_dump["jobs"])
+
+        params["limit"] = final_limit
+        final_jobs = re.get("https://himalayas.app/jobs/api", params=params)
+        final_dump = final_jobs.json()
+
+        initial_dump["jobs"].extend(final_dump["jobs"])
+
+
+            # f.write(",\n")
+            
+        # job_directory = []
+
+
+        # for j in site_dump["jobs"]:
+
+        #     job_directory.append(
+        #         {
+        #             "job-title": j["title"],
+        #             "employer": j["companyName"],
+        #             "description": j["description"],
+        #             "published-on": datetime.datetime.fromtimestamp(j["pubDate"]).strftime("%c"),
+        #             "link": j["guid"]
+        #         }
+        #     )
+            
+        if initial_dump["jobs"][-1] == []:
+            initial_dump["jobs"].pop()
+
+        with open("him.json", "w") as f:
+                json.dump(initial_dump, f, indent=4)
+
+        return initial_dump["jobs"]
+    else:
+        return
 
 def jobChecker(job_array):
-
+    """Returns a dict of the total count of skill occurrences in himalayas jobs"""
     result_set = []
 
     t = TrieNode()
@@ -36,10 +91,34 @@ def jobChecker(job_array):
         t.addPhrase(t, skill_dict[skill], skill)
 
     for job in job_array:
-        result_set.append(t.findPhrases(t, job["description"]))
-        
-    return result_set
+        t.findPhrases(t, job["description"], skill_count)
 
-job_list = jobGetter()
-print(job_list)
-print(jobChecker(job_list))
+    return skill_count
+
+def skilledJobCount(count):
+    """Returns a formatted dict of count of skill occurrence"""
+    final = {}
+
+    for i in range(1, len(skill_dict)):
+        final[skill_dict[i]] = count[i]
+
+
+    return final
+
+if __name__ == "__main__":
+    start = time.time()
+
+    with open("him.json", "r") as f:
+        job_list = json.load(f)["jobs"]
+        skillcount = jobChecker(job_list)
+
+        print(skillcount)
+
+        print(skilledJobCount(skillcount))
+
+    # job_list = jobGetter()
+    # print(jobChecker(job_list))
+
+
+    execution = time.time() - start
+    print("Executed in: {}".format(str(execution)))
--- backend/remotiveAPICaller.py
+++ backend/remotiveAPICaller.py
@@ -1,33 +1,49 @@
 import requests as re
 import datetime
 from trie import TrieNode
+import skills
+import json
 
-skill_dict = {1: "Python", 2: "Vertex", 3: "java", 4: "AWS"}
+d = {}
 
-def remotiveJobGetter():
-    job = re.get("https://remotive.com/api/remote-jobs?category=software-dev&limit=5")
-
-    site_dump = job.json()
-
-    job_directory = []
+skill_dict = skills.fullList(d)
+skill_count = {}
+for i in skill_dict:
+    skill_count[i] = 0
 
 
-    for j in site_dump["jobs"]:
+def remotiveJobGetter(state):
 
-        job_directory.append(
-            {
-                "job-title": j["title"],
-                "employer": j["company_name"],
-                "description": j["description"],
-                "published-on": datetime.datetime.fromisoformat(j["publication_date"]).strftime("%c"),
-                "link": j["url"]
-            }
-        )
+    if state == "offline":
+        with open("rem.json", "r") as f:
+            site_dump = json.load(f)
+    elif state == "online":
+        job = re.get("https://remotive.com/api/remote-jobs")
+        site_dump = job.json()
+    else:
+        return
 
-    return job_directory
+    # job_directory = []
+
+
+    # for j in site_dump["jobs"]:
+
+    #     job_directory.append(
+    #         {
+    #             "job-title": j["title"],
+    #             "employer": j["company_name"],
+    #             "description": j["description"],
+    #             "published-on": datetime.datetime.fromisoformat(j["publication_date"]).strftime("%c"),
+    #             "link": j["url"]
+    #         }
+    #     )
+
+    # return job_directory
+
+    return site_dump["jobs"]
 
 def remotiveJobChecker(job_array):
-
+    """Returns a dict of the total count of skill occurrences in remotive jobs"""
     result_set = []
 
     t = TrieNode()
@@ -36,10 +52,27 @@ def remotiveJobChecker(job_array):
         t.addPhrase(t, skill_dict[skill], skill)
 
     for job in job_array:
-        result_set.append(t.findPhrases(t, job["description"]))
+        t.findPhrases(t, job["description"], skill_count)
         
-    return result_set
+    return skill_count
 
-job_list = remotiveJobGetter()
-print(job_list)
-print(remotiveJobChecker(job_list))
+def skilledJobCount(count):
+    """Returns a formatted dict of count of skill occurrence"""
+    final = {}
+
+    for i in range(1, len(skill_dict)):
+        final[skill_dict[i]] = count[i]
+
+
+    return final
+
+# def getSkillJobs(skill, final_dict):
+    
+#     return final_dict[skill]
+
+if __name__ == "__main__":
+    # job_list = remotiveJobGetter()
+    # print(job_list)
+    with open("rem.json", "r") as f:
+        job_list = json.load(f)["jobs"]
+        print(remotiveJobChecker(job_list))
